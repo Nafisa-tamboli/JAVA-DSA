9dc4359771126e56b868aeefe823ed8184f55050
Nafisa-tamboli <142076132+Nafisa-tamboli@users.noreply.github.com>
2025-04-10 11:34:47 +0530

    initial commit

---
+++ DoWhileDemo.java
@@ -0,0 +1,9 @@
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Running at least once! i = " + i);
+            i++;
+        } while(i <= 3);
+    }
+}
